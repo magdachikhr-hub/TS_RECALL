7feaea3bc46a21be054038ef4901f5d8ea40f143
magda chikhradze <magda292005@gmail.com>
2026-09-21 13:55:41 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let text = "yoyoyoyo";
+// text = {}
+console.log(text + 1);
+let numb = 56;
+// numb = []
+let isChecked = true || false;
+let isCheck = true || false;
+let planets = [
+    "earth",
+    "mars",
+    "pluto",
+    {},
+    [],
+];
+function sum() { }
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,22 @@
+let text: string = "yoyoyoyo";
+
+// text = {}
+
+console.log(text + 1);
+
+let numb: number = 56;
+
+// numb = []
+
+let isChecked: true | false = true || false;
+let isCheck: boolean = true || false;
+
+let planets: (string | {} | [] | boolean)[] = [
+  "earth",
+  "mars",
+  "pluto",
+  {},
+  [],
+];
+
+function sum() {}
