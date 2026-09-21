a0836f44483aff5215e3403c75fdd8316f56e0ed
magda chikhradze <magda292005@gmail.com>
2026-09-21 15:22:19 +0400

    wip

--- main.js
+++ main.js
@@ -30,9 +30,17 @@ let state;
 let random = number < 0 && "entr";
 let random2 = number < 0 || "string";
 let random3 = state ?? 54;
+let random4 = 0 || number > 0;
+//0 on the left is considered falsy
 console.log(random);
 console.log(random2);
 console.log(random3);
-export {};
+console.log(random4);
 //?? - checks if "state" is undefined - if it is undefined it looks whats next to it
+const num2 = 59495;
+let random5 = num2 ?? 54;
+let random6 = num2 || "string";
+console.log(random5);
+console.log(random6);
+export {};
 //# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -56,10 +56,23 @@ let random2 = number < 0 || "string";
 
 let random3 = state ?? 54;
 
+let random4 = 0 || number > 0;
+//0 on the left is considered falsy
+
 console.log(random);
 
 console.log(random2);
 
 console.log(random3);
 
+console.log(random4);
+
 //?? - checks if "state" is undefined - if it is undefined it looks whats next to it
+
+const num2 = 59495;
+
+let random5 = num2 ?? 54;
+let random6 = num2 || "string";
+
+console.log(random5);
+console.log(random6);
